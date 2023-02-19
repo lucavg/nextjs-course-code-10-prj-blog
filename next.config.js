69b0cf108c92d937aab3c4908b5edb6efc8d5288
Luca Van Genechten <r0849476@student.thomasmore.be>
2023-02-19 22:36:56 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -11,35 +11,3 @@ module.exports = {
     mongodb_database: process.env.mongodb_database,
   },
 };
-
-// module.exports = (phase) => {
-//   if (phase === PHASE_DEVELOPMENT_SERVER) {
-//     return {
-//       env: {
-//         mongodb_username: "exorra",
-//         mongodb_password: "rN53bOasKMgMb6QI",
-//         mongodb_clustername: "cluster0",
-//         mongodb_database: "my-site-dev",
-//       },
-//     };
-//   }
-//   if (phase === PHASE_PRODUCTION_SERVER) {
-//     return {
-//       env: {
-//         mongodb_username: process.env.mongodb_username,
-//         mongodb_password: process.env.mongodb_password,
-//         mongodb_clustername: process.env.mongodb_clustername,
-//         mongodb_database: process.env.mongodb_database,
-//       },
-//     };
-//   }
-
-//   return {
-//     env: {
-//       mongodb_username: "exorra",
-//       mongodb_password: "rN53bOasKMgMb6QI",
-//       mongodb_clustername: "cluster0",
-//       mongodb_database: "my-site",
-//     },
-//   };
-// };
